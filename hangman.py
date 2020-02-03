9309184aab09e39c5a651f79cddd0703f0882daa
Dennis Abraham <bashnewb@gmail.com>
2020-02-04 00:29:58 +0530

    Complete draft (working code)

--- hangman.py
+++ hangman.py
@@ -1,6 +1,6 @@
 
 
-def find_all_indexes(input_str, search_str):
+def find_indices_of_substr(input_str, search_str):
     indices = []
     length = len(input_str)
     index = 0
@@ -15,7 +15,7 @@ def find_all_indexes(input_str, search_str):
 
 word = "chocolate"
 toDisplay=""
-correctAttempt=True
+completed=False
 
 length_word = len(word)
 
@@ -24,29 +24,31 @@ for x in range(length_word):
 
 i = 0
 
-no_of_tries = 3 
+no_of_tries = 8
 
 print ("Hint: Sweet")
 
-while (i < no_of_tries and ("*"  in toDisplay) ):
-    while (correctAttempt):
+while (i < no_of_tries and  not completed ):
+    #while (correctAttempt):
         print (toDisplay)
         input_char=input("Guess a character > ")
-        
+
         if input_char[0] in word:
-            locs_of_char=find_all_indexes(word,input_char[0])
+            locs_of_char=find_indices_of_substr(word,input_char[0])
             toDisplay=list(toDisplay)
             for loc in locs_of_char:
                 toDisplay[loc]=input_char[0]
             toDisplay="".join(toDisplay)
+            if "*" not in toDisplay:
+                completed=True
         elif input_char[0] not in word:
-            correctAttempt = False
+            #correctAttempt = False
             i=i+1
             attempts=no_of_tries - i
             print ("You have ",attempts," attempts left")
             correctAttempt = True
-            break
-        
 
-
-    
+if completed:
+    print ("You have won the round !! ")
+else:
+    print ("You have exhausted all your attempts. Better luck next time ")
